386902012773e15c6f22342af221ec899aa0723b
Oscar Busk <oscar.busk@gmail.com>
2026-04-07 23:46:57 +0200

    Build executable from cli

--- packages/cli/build.ts
+++ packages/cli/build.ts
@@ -1,9 +1,17 @@
 export {};
 
-await Bun.build({
-  entrypoints: ["src/index.ts"],
-  target: "node",
-  outdir: "dist",
-  banner: "#!/usr/bin/env node",
-  external: ["@dnscmp/core"],
-});
+await Promise.all([
+  Bun.build({
+    entrypoints: ["src/index.ts"],
+    target: "node",
+    outdir: "dist",
+    banner: "#!/usr/bin/env node",
+    external: ["@dnscmp/core"],
+  }),
+  Bun.build({
+    entrypoints: ["src/index.ts"],
+    compile: {
+      outfile: "release/dnscmp",
+    },
+  }),
+]);
